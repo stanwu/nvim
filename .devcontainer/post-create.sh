c8d1a4ee3b4c11859c3708305970d123008f73a7
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-15 05:49:52 +0000

    Init

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,5 +4,4 @@ sudo apt-get update && \
         sudo apt-get update && \
         sudo apt-get install -y make gcc ripgrep unzip git xclip neovim zsh && \
         git clone https://github.com/nvim-lua/kickstart.nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim && \
-        chsh -s /bin/zsh && \
-        zsh
+        chsh -s /bin/zsh
