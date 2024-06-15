f3daa16607509d110073208ad638a037d80503db
Stan Ht. Wu <stanwu.tw@gmail.com>
2024-06-15 06:07:34 +0000

    Init

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,5 +3,6 @@ sudo apt-get update && \
         sudo add-apt-repository ppa:neovim-ppa/unstable -y && \
         sudo apt-get update && \
         sudo apt-get install -y make gcc ripgrep unzip git xclip neovim zsh && \
-        git clone https://github.com/nvim-lua/kickstart.nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim && \
-        chsh -s /bin/zsh
+        git clone https://github.com/nvim-lua/kickstart.nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
+
+        
